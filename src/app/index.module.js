ff7f47f17a8d7f2531f2740e05618af46099bfb5
Mark Koh <mkoh@rjmetrics.com>
2015-10-03 04:22:57 +0000

    Did the stuff with the-THE THING, MORTY!

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('rmdrink', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+    .module('rmdrink', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngResource', 'ui.router', 'ngMaterial', 'toastr', 'angulartics', 'angulartics.google.analytics']);
 
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,97 +6,108 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, toastr) {
+  function MainController($timeout, $analytics) {
     var vm = this;
+
+    $analytics.pageTrack('/');
+
+    vm.rickedness = 1;
+
+    vm.ricked = function(rule) {
+      if (vm.rickedness >= rule.level){
+        return true;
+      }
+      return false;
+    };
 
     vm.rules = [
       {
         rule: "Any time Rick burps",
-        levels: []
+        level: 3
       },
       {
         rule: "Any time Rick drinks",
-        levels: []
+        level: 2
       },
       {
         rule: "Any time Morty says \"Uhhh...Rick\"",
-        levels: []
+        level: 2
       },
       {
         rule: "Any time someone goes through a portal",
-        levels: []
+        level: 1
       },
       {
         rule: "WUBBALUBBADUBDUB!!!!",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time Jerry does/says anything dumb",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time there's a reference to Morty being \"slow\"",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time Rick makes a new invention",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time a fight scene begins",
-        levels: []
+        level: 2
       },
       {
         rule: "Any time Beth drinks wine",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time Rick and Morty gets real",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time they break the 4th wall",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time a named character dies",
-        levels: []
+        level: 3
       },
       {
         rule: "Any time they talk about Beth being a horse surgeon",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time Summer gets annoyed",
-        levels: []
+        level: 2
       },
       {
         rule: "Any time Morty wants to bail",
-        levels: []
+        level: 2
       },
       {
         rule: "Any time they mention a dimension",
-        levels: []
+        level: 3
       },
       {
         rule: "Any time they say the name of the epiode",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time they spit on someone",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time Morty or Summer gets it in",
-        levels: []
+        level: 1
       },
       {
         rule: "Any time Morty blows up",
-        levels: []
+        level: 1
       },
       {
         rule: "Mr. Poopybutthole = FINISH YOUR DRINK",
-        levels: []
+        level: 2
       }
     ];
 
