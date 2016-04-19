07f605b1aea0b586e45a185d8067f5cf5bb526e3
Mark Koh <mkoh@drexel.edu>
2016-04-19 04:47:34 +0000

    Added some constants

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -43,7 +43,7 @@
         level: 1
       },
       {
-        rule: "Any time Rick makes a new invention",
+        rule: "Any time Rick whips out a zaneay invention",
         level: 1
       },
       {
@@ -107,107 +107,95 @@
         level: 2
       }
     ])
-    .constant('rulez420',[
+    .constant('rulez420', [
       {
-        rule: "Any time Rick burps",
-        level: 3
+        rule: "Any time someone says \"you gotta relax\"",
+        level: 420,
+        who: 0
       },
       {
-        rule: "Any time Rick drinks",
-        level: 2
-      },
-      {
-        rule: "Any time Rick or Morty stutter",
-        level: 3
-      },
-      {
-        rule: "Any time Morty says \"Uhhh...Rick\" or \"Aw geez, Rick\"",
-        level: 2
-      },
-      {
-        rule: "Any time someone goes through a portal",
-        level: 1
-      },
-      {
-        rule: "WUBBALUBBADUBDUB!!!!",
-        level: 1
-      },
-      {
-        rule: "Any time Jerry does/says anything dumb",
-        level: 1
-      },
-      {
-        rule: "Any time anyone says \"bitch\", BITCH!",
-        level: 3
-      },
-      {
-        rule: "Any time there's a reference to Morty being \"slow\"",
-        level: 1
-      },
-      {
-        rule: "Any time Rick makes a new invention",
-        level: 1
-      },
-      {
-        rule: "Any time a fight scene begins",
-        level: 2
-      },
-      {
-        rule: "Any time Beth drinks wine",
-        level: 1
-      },
-      {
-        rule: "Any time Rick and Morty gets real",
-        level: 1
-      },
-      {
-        rule: "Any time a penis is drawn into the scene",
-        level: 2
+        rule: "Any time Rick loses his portal gun",
+        level: 420,
+        who: 0
       },
       {
         rule: "Any time they break the 4th wall",
-        level: 1
+        level: 420,
+        who: 0
       },
       {
-        rule: "Any time a named character dies",
-        level: 3
+        rule: "Any time Morty says \"Uhhh...Rick\" or \"Aw geez, Rick\"",
+        level: 420,
+        who: 0
       },
       {
-        rule: "Any time they talk about Beth being a horse surgeon",
-        level: 1
+        rule: "Any time they show rainbow suspenders guy!",
+        level: 420,
+        who: 0
       },
       {
-        rule: "Any time Summer gets annoyed",
-        level: 2
+        rule: "WUBBALUBBADUBDUB!!!!",
+        level: 420,
+        who: 0
       },
       {
-        rule: "Any time Morty wants to bail",
-        level: 2
+        rule: "Any time Jerry is a disappoint",
+        level: 420,
+        who: 0
       },
       {
-        rule: "Any time they mention a dimension",
-        level: 3
+        rule: "Any time anyone says \"bitch\", BITCH!",
+        level: 1,
+        who: 0
       },
       {
-        rule: "Any time they say the name of the epiode",
-        level: 1
+        rule: "Any time there's a reference to Morty being \"slow\"",
+        level: 1,
+        who: 0
       },
       {
-        rule: "Any time they spit on someone",
-        level: 1
+        rule: "Any time Rick whips out a zany invention",
+        level: 1,
+        who: 0
+      },
+
+
+
+      {
+        rule: "Any time you just totally forget what's going on",
+        level: 420,
+        who: 1
       },
       {
-        rule: "Any time Morty or Summer gets it in",
-        level: 1
+        rule: "Any time you pause the show",
+        level: 420,
+        who: 1
       },
       {
-        rule: "Any time Morty blows up",
-        level: 1
+        rule: "Any time you giggle before the joke even starts",
+        level: 420,
+        who: 1
       },
       {
-        rule: "Mr. Poopybutthole = FINISH YOUR DRINK",
-        level: 2
-      }
+        rule: "Any time they smoke anything",
+        level: 420,
+        who: 0
+      },
+      {
+        rule: "Any time a penis is drawn into the scene",
+        level: 420,
+        who: 0
+      },
+      {
+        rule: "Any time you recognize a random alien species",
+        level: 420,
+        who: 1
+      },
+      {
+        rule: "Any time you unintentionally quote the show",
+        level: 420,
+        who: 1
+      },
     ]);
 
 })();
